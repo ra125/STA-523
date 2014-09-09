6bd4b1fcf45adf7358d333d063cadd19c3d02541
Kungang Zhang <kz29@saxon.stat.duke.edu>
2014-09-09 00:29:08 -0400

    Copy the explain to R

--- Sept8-exe2.R
+++ Sept8-exe2.R
@@ -23,5 +23,6 @@ d[,5][d[,5]==1]='upper division'
 d[d==-999.0]=NA
 
 #mean of evalutation
+#if use (d[,1][d[,1]==NA]), it wouldn't work, becasuse there is a miss value there. In this form, all the value return would be "missing value", i.e. NA.
 mean(d[,1][!is.na(d[,1])])
 mean(d[,2][!is.na(d[,2])])
