b19482abc08620001b48f65e3d90b349e3bf42a7
Sophie Lee <sophie@Sophies-MacBook-Air.local>
2014-10-03 16:05:24 -0400

    separating the code

---
+++ HW2/dennys_parse.R
@@ -0,0 +1,56 @@
+
+
+
+
+names=c("UT","DC","KS","AL","HI")
+
+
+for(i in names){
+
+load("dennys/names.json")
+
+# Clean up jQuery wrapper to get valid json
+
+each=as.character(str_match_all(s,"<name>(.*?)</uid>"))
+name=as.data.frame(str_match_all(each,"name>(.*?)</name>"))
+name=as.data.frame(name[1:1000,2])
+uid=as.data.frame(str_match_all(each,"<uid>(.*?)</uid>"))
+uid=as.data.frame(uid[1:1000,2])
+address1=as.data.frame(str_match_all(each,"<address1>(.*?)</address1>"))
+address1=as.data.frame(address1[1:1000,2])
+address2=as.data.frame(str_match_all(each,"<address2>(.*?)</address2>"))
+address2=as.data.frame(address2[1:1000,2])
+city=as.data.frame(str_match_all(each,"<city>(.*?)</city>"))
+city=as.data.frame(city[1:1000,2]) 
+state=as.data.frame(str_match_all(each,"<state>(.*?)</state>"))
+state=as.data.frame(state[1:1000,2])
+country=as.data.frame(str_match_all(each,"<country>(.*?)</country>"))
+country=as.data.frame(country[1:1000,2])
+zip=as.data.frame(str_match_all(each,"<postalcode>(.*?)</postalcode>"))
+zip=as.data.frame(zip[1:1000,2])
+phone=as.data.frame(str_match_all(each,"<phone>(.*?)</phone>"))
+phone=as.data.frame(phone[1:1000,2])
+fax=as.data.frame(str_match_all(each,"<fax>(.*?)</fax>"))
+fax=as.data.frame(fax[1:1000,2])
+latitude=as.data.frame(str_match_all(each, "<latitude>([0-9.0-9]*)</latitude>"))
+latitude=as.data.frame(latitude[1:1000,2])
+longitude=as.data.frame(str_match_all(each,"<longitude>(-[0-9.0-9]*)</longitude>"))
+longitude=as.data.frame(longitude[1:1000,2])
+
+data=cbind("uid"=uid, "state"=state, "address1"=address1, "address2"=address2,"phone"=phone, "latitude"=lat, "longitude"=long)
+assign(i, data)
+
+
+}
+
+
+################### add them together and make the final data
+
+data.dennys=rbind(data.UT, data.DC, data.AL, data.HI, data.KS)
+data.dennys=data.dennys[!duplicated(data.dennys$uid),]
+
+
+
+#table(data.dennys[,2])
+
+
