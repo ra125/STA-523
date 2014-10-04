a72632b1cbbc5ea75c515a822ffd30aecd85c6c4
Kungang Zhang <kz29@saxon.stat.duke.edu>
2014-10-04 19:40:46 -0400

    Delete the first paragraph in hw2.Rmd

--- HW2/lq_hotel_download.R
+++ HW2/lq_hotel_download.R
@@ -7,15 +7,14 @@ load(file="lq/list.Rdata")
 
 dir.create("lq/hotels/", showWarnings = FALSE)
 
-# Loop over hotels and download their pages using their specific "innNumber" in the url
-num_hotels = length(dataframe[,2])
+# Loop over hotels and download their pages
+num_hotels = length(dataframe[,1])
 for(i in 1:num_hotels)
 {
   url = paste0("http://www.lq.com/en/findandbook/hotel-details.",dataframe[i,"innNumber"],".address.html")
   page = GET(url)
-  stopifnot(page$status_code == 200)
   data = content(page, as="text")
   print(url)
   write(data, file=paste0("lq/hotels/",dataframe[i,"innNumber"],".html"))
-  Sys.sleep(1) # wait before grabbing the next page
+  #Sys.sleep(1) # wait before grabbing the next page
 }
