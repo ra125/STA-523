020bea530786e9b850ac14eff9599875b6b55cdf
Sophie Lee <sophie.jiseon.lee@gmail.com>
2014-10-08 17:52:46 -0400

    exercise

---
+++ inclass/1008_Ex.R
@@ -0,0 +1,26 @@
+### This relies on C, which consumes a lot of RAM. Save your work as you go.
+
+library(data.table)
+install.packages("dplry")
+library(dplyr)
+park = fread("/home/vis/cr173/Sta523/data/parking/NYParkingViolations.csv",sep=",")
+head(park)
+
+str(transmute(park, dt=paste(Issue.Date,Violation.Time)) %>% mutate(dt=ymd_hm(dt)) 
+    %>% mutate(hour=hour(dt), wday=as.character(wday(dt,late=TRUE ))) )
+
+date=(transmute(park, dt=paste(Issue.Date,Violation.Time)) %>% mutate(dt=ymd_hm(dt)) 
+      %>% mutate(hour=hour(dt), wday=as.character(wday(dt,late=TRUE )))
+      
+group_by(date, wday) %>% summarize(n())
+
+group_by(date, hour) %>% summarize(n())
+
+group_by(date, hour, wday) %>% summarize(n())
+
+which.max(hourwday$`n()`)
+
+    precinct<-park$"Violation Location"
+house_street<-cbind(park$"House Number", park$"Street Name")
+address<-apply(house_street,1,paste,collapse=" ")
+
