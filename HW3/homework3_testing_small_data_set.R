b18febfa4bbd9c993d845e10921718f3a0b82789
Kungang Zhang <kz29@saxon.stat.duke.edu>
2014-10-24 15:41:48 -0400

    use all data

--- HW3/homework3_testing_small_data_set.R
+++ HW3/homework3_testing_small_data_set.R
@@ -7,7 +7,7 @@ rm(check_packages)  #Remove anything that could take up memory
 
 #Load the Biggish NYC parking violations datafile
 #test with small subset of data first
-park = fread("/home/vis/cr173/Sta523/data/parking/NYParkingViolations_small.csv",sep=",")
+park = fread("/home/vis/cr173/Sta523/data/parking/NYParkingViolations.csv",sep=",",nrows=35145)
 park<-park[park$'Violation County'=="NY",] #Filtering only NY for Manhattan
 park$'Issue Date'=mdy(park$'Issue Date') 
 colnames(park$'Issue Date')="Issue.Date" 
@@ -57,3 +57,46 @@ rm(basepath)
 #### We exlude observations that are not associated with the correct Manhattan precincts (1:34)
 d<-d[which(d[,1] < 35),]
 pluto<-pluto[which(pluto$precinct<35 & pluto$precinct>0),]
+
+
+#make the data content consistent, the current two targets are addr$addr and pluto$Address
+pluAddrlow<-tolower(pluto$Address)
+addraddr<-addr$addr
+
+namemap<-rbind(c("east","west","street","avenue","road","drive","terrace"), c("e","w","st","ave","rd","dr","ter"))
+lnm<-ncol(namemap)
+
+for (i in 1:lnm)
+{
+  pluAddrlow<-str_replace_all(pluAddrlow, namemap[1,i], namemap[2,i])
+  addraddr<-str_replace_all(addraddr, namemap[1,i], namemap[2,i])
+}
+
+
+pluAddrlow<-str_replace_all(pluAddrlow, namemap[1,], namemap[2,])
+
+  
+
+pluAddrlow<-str_replace_all(pluAddrlow, namemap[1,1],namemap[2,1])
+pluAddrlow<-str_replace_all(pluAddrlow, "west","w")
+pluAddrlow<-str_replace_all(pluAddrlow, "street","st")
+pluAddrlow<-str_replace_all(pluAddrlow, "avenue","ave")
+pluAddrlow<-str_replace_all(pluAddrlow, "road","rd")
+
+addraddr<-str_replace_all(addraddr, "east","e")
+pluAddrlow<-str_replace_all(pluAddrlow, "west","w")
+pluAddrlow<-str_replace_all(pluAddrlow, "street","st")
+pluAddrlow<-str_replace_all(pluAddrlow, "avenue","ave")
+pluAddrlow<-str_replace_all(pluAddrlow, "road","rd")
+
+sum(str_count(pluAddrlow,"brdway"))
+sum(str_count(addraddr,"broadway"))
+
+fruits <- c("one apple", "two pears", "three bananas")
+str_replace(fruits, "[aeiou]", "-")
+str_replace_all(fruits, "[aeiou]", "-")
+
+str_replace_all(fruits, "([aeiou])", "")
+str_replace_all(fruits, "([aeiou])", "\\1\\1")
+str_replace_all(fruits, "[aeiou]", c("1", "2", "3"))
+str_replace_all(fruits, c("a", "e", "i"), "-")
