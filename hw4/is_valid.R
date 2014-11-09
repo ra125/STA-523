f9ae304f97b8b3256a61d5b4829aa5ef681356b8
Radhika Anand <ra125@duke.edu>
2014-11-08 23:52:20 -0500

    Cleaning up of extra spaces

--- hw4/is_valid.R
+++ hw4/is_valid.R
@@ -1,4 +1,3 @@
-
 is_valid=function(input){
   
   # 1: Testing if the input has a list object
@@ -21,43 +20,30 @@ is_valid=function(input){
   }
   
   
-  
-  
   # 2: Testing if the names of the lists in object are all unique
   if(  length(names(input))==0   )   {  #begin first ifelse
     t2<-TRUE
   } else { 
-    
     if(as.character( length(unique(names(input)))==length(input)) =="TRUE"){  #begin second ifelse
       t2<-TRUE
     } else{
       return(F)
-    }                  #end second ifelse
-    
+    }                  #end second ifelse 
   } #end first ifelse
   
   
-  
-    
-  
   # 3: Testing if the object has both weights and edges as lists
-  if(as.character( sum(grep("weights",names(input[[1]])))>0 & sum(grep("edges",names(input[[1]])))>0 )=="TRUE"){
+  if(as.character(sum(grep("weights",names(input[[1]])))>0 & sum(grep("edges",names(input[[1]])))>0 )=="TRUE"){
     t3<-TRUE
   } else{
     return(F)
   }
   
   
-  
-  
-  
   # 4: Testing if the types of edges are integer and weights are double
-  
   if(length(input[[1]]$edges)==0) {
     t4<-TRUE
-  } else{
-    
-    
+  } else{ 
     n=length(input)
     t_obj4_1 =data.frame(table(is.na(input[[n]]$edges) ) )
     t_obj4_2 =t_obj4_1[which(t_obj4_1[,1]==TRUE),2]
@@ -66,33 +52,29 @@ is_valid=function(input){
       t4<-FALSE
     } else{
       
-      if(as.character(  typeof(input[[1]]$edges)  == "integer" & typeof(input[[1]]$weights)=="double")=="TRUE"){
+      if(as.character(typeof(input[[1]]$edges)  == "integer" & typeof(input[[1]]$weights)=="double")=="TRUE"){
         t4<-TRUE
       } else{
         return(F)
-      }
-      
+      } 
     }
-    
   }
   
-  #5: Testing if there are not any edges to non-existent vertices. 
   
-
-     n=length(input)
-      temp=NULL
-      t_obj5_1=list()
-      t_obj5_2=(1:n)
+  # 5: Testing if there are not any edges to non-existent vertices.   
+  n=length(input)
+  temp=NULL
+  t_obj5_1=list()
+  t_obj5_2=(1:n)
       
-      for(i in 1:n){
+  for(i in 1:n){
         t_obj5_1[[i]]<-input[[i]]$edges
         temp=c(temp, t_obj5_1[[i]])    #temp; directed edges. the list number of these numbers should exist
                     } 
 
      if(length(temp)==0){   #First ifelse: If there is no edge numbers, the object passes this test.
        t5<-TRUE
-     } else {
-       
+     } else {  
        if(is.na(temp)[1]==TRUE){ # Second ifelse; no else condition
          return(F)
        }
@@ -104,7 +86,6 @@ is_valid=function(input){
        for(i in 1:n){
         dt[i,1]=is.integer(input[[i]]$edges)    #binary vector: if 1, such vertex exists
               }
-      
            TF=matrix(nrow=max(unique(c(temp,n))))
            for(i in temp){
             TF[i,]<- dt[i,1]=="TRUE"
@@ -120,25 +101,17 @@ is_valid=function(input){
       if(length(test)>0){        #begin 4th if else
         return(F)
       }     else {
-
         t5=TRUE
        }  ## end 4th ifelse 
-    
-
     }    # end First ifelse
     
 
-  
-  
-  
   # 6: Testing if the weights are greater than 0.
-  if(   typeof( length(input[[1]]$weights))=="integer"  ){
-    
+  if(typeof( length(input[[1]]$weights))=="integer"  ){  
     
     if( length(input[[1]]$weights)==0 ){
       t6<-TRUE
     }    else{
-      
       t_obj6<-as.character(input[[1]]$weights>0)
       t_obj6_1 <-data.frame(table(t_obj6))
       int<-t_obj6_1[which(t_obj6_1[,1]==TRUE),2]
@@ -150,13 +123,10 @@ is_valid=function(input){
       }  else{
        return(F)              
       }
-      
     }
   } else{
     return(F)
   }
-  
-  
   
   
   # 7: Testing if edges and weights have the same length
@@ -173,9 +143,6 @@ is_valid=function(input){
   } else{
     t7<-TRUE
   }
-  
-  
-  
   
   
   #8: Testing if there are duplicate edges
@@ -195,8 +162,6 @@ is_valid=function(input){
   }
   
   
-  
-  
   # Testing if the object passes all tests above
   each<-cbind(t1,t2,t3,t4,t5,t6, t7, t8)
   if(sum(which(each==TRUE))==36) {
@@ -204,6 +169,5 @@ is_valid=function(input){
   } else{
     return(F)
   }
-
 
 }
